03ec7a0b259b3817a04b1b8f4e9fb751df0f57fc
Krishanjit Rajbongshi <84830284+krishanjit19@users.noreply.github.com>
2024-01-18 02:32:13 +0530

    Update Patient.java

--- src/main/java/com/example/q3/model/Patient.java
+++ src/main/java/com/example/q3/model/Patient.java
@@ -2,6 +2,8 @@ package com.example.q3.model;
 
 public class Patient {
     private Long id;
+    private String name;
+    private int age;
 
     public Long getId() {
         return id;
@@ -11,5 +13,28 @@ public class Patient {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
