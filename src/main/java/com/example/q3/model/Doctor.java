bcccb4a20d4ff9461fd017db3afbca80ada5c9e4
Krishanjit Rajbongshi <84830284+krishanjit19@users.noreply.github.com>
2024-01-18 02:32:37 +0530

    Update Doctor.java

--- src/main/java/com/example/q3/model/Doctor.java
+++ src/main/java/com/example/q3/model/Doctor.java
@@ -2,6 +2,8 @@ package com.example.q3.model;
 
 public class Doctor {
     private Long id;
+    private String name;
+    private String specialization;
 
     public Long getId() {
         return id;
@@ -9,5 +11,30 @@ public class Doctor {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", specialization='" + specialization + '\'' +
+                '}';
     }
 }
